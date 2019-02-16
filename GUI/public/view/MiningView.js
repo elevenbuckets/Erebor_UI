ba3e857e986158b1ebf9c454105c6fa82a82ba5a
liang <zh_l2006@sina.com>
2019-02-15 21:42:14 -0700

    add more mining component

--- GUI/public/view/MiningView.js
+++ GUI/public/view/MiningView.js
@@ -34,7 +34,74 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 class HeaderBarView extends _reflux2.default.Component {
 	constructor(props) {
 		super(props);
-		this.store = _EreborStore2.default;
+
+		this.updateView = view => {
+			this.props.updateView(view);
+		};
+
+		this.__renderMiningMessages = () => {
+			return this.state.currentMiningMessages.map(message => {
+				return _react2.default.createElement(
+					'div',
+					null,
+					message
+				);
+			});
+		};
+
+		this.__renderMiningSettings = () => {
+			return _react2.default.createElement(
+				'div',
+				null,
+				_react2.default.createElement(
+					'div',
+					{ className: 'headerbarButton', style: { color: this.props.currentView === 'AppLauncher' ? '#ff4200' : 'white' },
+						onClick: this.updateView.bind(this, 'AppLauncher') },
+					'Gamer'
+				),
+				_react2.default.createElement(
+					'div',
+					{ className: 'headerbarButton', style: { color: this.props.currentView === 'TokenSettings' ? '#ff4200' : 'white' },
+						onClick: this.updateView.bind(this, 'TokenSettings') },
+					'Defender'
+				),
+				_react2.default.createElement(
+					'div',
+					{ className: 'headerbarButton', style: { color: this.props.currentView === 'Receipts' ? '#ff4200' : 'white' },
+						onClick: this.updateView.bind(this, 'Receipts') },
+					'Validator'
+				),
+				this.__renderMiningRole(this.state.miningRole)
+			);
+		};
+
+		this.__renderMiningRole = role => {
+			return role === "Gamer" ? _react2.default.createElement(
+				'div',
+				{ className: 'gamerSetting' },
+				_react2.default.createElement(
+					'label',
+					{ className: 'item TransferTo', style: { border: 'none' } },
+					'Addr:',
+					_react2.default.createElement('input', { size: 30, type: 'text', style: {
+							backgroundColor: "rgba(255,255,255,0)",
+							border: "1px solid white",
+							color: "white",
+							fontWeight: "bold",
+							fontSize: "24px",
+							fontFamily: "monospace",
+							textAlign: "center"
+						},
+						value: this.state.recipient, placeholder: 'Ethereum Address' })
+				),
+				_react2.default.createElement('input', { type: 'button', className: 'button', style: { margin: "40px 0 0 40px", fontSize: "22px" }, value: 'start', onClick: this.handleSend })
+			) : _react2.default.createElement('div', null);
+		};
+
+		this.state = {
+			currentMiningMessages: ["Currently mining, the expected mined time is 10 min,", "Keep going"],
+			miningRole: "Gamer"
+		};
 	}
 
 	render() {
@@ -42,7 +109,16 @@ class HeaderBarView extends _reflux2.default.Component {
 		return _react2.default.createElement(
 			'div',
 			{ className: 'mining' },
-			'Currently mining, the expected mined time is 10 min'
+			_react2.default.createElement(
+				'div',
+				{ className: 'miningMessages' },
+				this.__renderMiningMessages()
+			),
+			_react2.default.createElement(
+				'div',
+				{ className: 'miningSettings' },
+				this.__renderMiningSettings()
+			)
 		);
 	}
 }
--- GUI/src/view/MiningView.js
+++ GUI/src/view/MiningView.js
@@ -16,17 +16,69 @@ import EreborActions from '../action/EreborActions';
 class HeaderBarView extends Reflux.Component {
 	constructor(props) {
 		super(props);
-		this.store = EreborStore;
+		this.state = {
+			currentMiningMessages: ["Currently mining, the expected mined time is 10 min,", "Keep going"],
+			miningRole: "Gamer"
+		}
+	}
+
+	updateView = (view) => {
+		this.props.updateView(view);
+	}
+	__renderMiningMessages = () => {
+		return this.state.currentMiningMessages.map(message => {
+			return <div>
+				{message}
+			</div>
+		})
+	}
+
+	__renderMiningSettings = () => {
+		return <div>
+			<div className="headerbarButton" style={{ color: this.props.currentView === 'AppLauncher' ? '#ff4200' : 'white' }}
+				onClick={this.updateView.bind(this, 'AppLauncher')}>Gamer</div>
+			<div className="headerbarButton" style={{ color: this.props.currentView === 'TokenSettings' ? '#ff4200' : 'white' }}
+				onClick={this.updateView.bind(this, 'TokenSettings')}>Defender</div>
+			<div className="headerbarButton" style={{ color: this.props.currentView === 'Receipts' ? '#ff4200' : 'white' }}
+				onClick={this.updateView.bind(this, 'Receipts')}>Validator</div>
+			{this.__renderMiningRole(this.state.miningRole)}
+		</div>;
+	}
+
+	__renderMiningRole = (role) => {
+		return role === "Gamer" ? <div className="gamerSetting">
+			<label className="item TransferTo" style = {{border:'none'}}>
+				Addr:
+		<input size={30} type='text' style=
+					{{
+						backgroundColor: "rgba(255,255,255,0)",
+						border: "1px solid white",
+						color: "white",
+						fontWeight: "bold",
+						fontSize: "24px",
+						fontFamily: "monospace",
+						textAlign: "center"
+					}} 
+					value={this.state.recipient} placeholder="Ethereum Address" />
+
+			</label>
+			<input type="button" className="button" style={{margin: "40px 0 0 40px", fontSize: "22px"}} value="start" onClick={this.handleSend} />
+		</div> : <div></div>
 	}
 
 	render() {
 		//console.log("In MainView render()");
 		return (
 			<div className="mining">
-				Currently mining, the expected mined time is 10 min
+				<div className="miningMessages">
+					{this.__renderMiningMessages()}
+				</div>
+				<div className="miningSettings">
+					{this.__renderMiningSettings()}
+				</div>
 			</div>
-			
-			
+
+
 		)
 
 	}
